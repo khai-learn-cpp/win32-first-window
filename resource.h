1435fa9273e54cec269daad61ea97dbf3635ca23
khai96_ <hvksmr1996@gmail.com>
2016-03-23 02:11:08 +0700

    update resource

--- resource.h
+++ resource.h
@@ -6,9 +6,10 @@
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-// #define IDI_APPLICATION			107
+#ifndef IDI_APPLICATION
+#   define IDI_APPLICATION		107
+#endif
 #define IDI_SMALL				108
-// #define IDI_APPLICATION			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
